0b3e44b863815472b4cb6d7d6bdb2e6120aa0552
marinamsm <marinasouza.msm@gmail.com>
2020-06-27 11:00:13 -0300

    Repository to create and list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionCreation {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +19,67 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): object {
+    return { transactions: this.transactions, balance: this.getBalance() };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const resultingBalance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            accumulator.total += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            accumulator.total -= transaction.value;
+            break;
+          default:
+            console.log('Something is wrong...');
+        }
+
+        return resultingBalance;
+      },
+      resultingBalance,
+    );
+
+    return resultingBalance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public findByWholeObject({
+    title,
+    value,
+    type,
+  }: TransactionCreation): Transaction | null {
+    const newTransaction = new Transaction({ title, value, type });
+
+    const existentTransaction = this.transactions.find(
+      transaction =>
+        transaction.title === newTransaction.title &&
+        transaction.value === newTransaction.value &&
+        transaction.type === newTransaction.type,
+    );
+
+    if (existentTransaction) {
+      return existentTransaction;
+    }
+
+    return null;
+  }
+
+  public create({ title, value, type }: TransactionCreation): Transaction {
+    const newTransaction = new Transaction({ title, value, type });
+
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
