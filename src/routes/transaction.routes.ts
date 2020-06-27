546f159b5db5cbcca35ec3a0bb15d6d9ba165032
marinamsm <marinasouza.msm@gmail.com>
2020-06-27 11:01:38 -0300

    Routes to call the transaction endpoints

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import ListTransactionService from '../services/ListTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactionsListService = new ListTransactionService(
+      transactionsRepository,
+    );
+
+    const transacions = transactionsListService.execute();
+
+    return response.json(transacions);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +24,15 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const transactionCreation = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const newTransaction = transactionCreation.execute({ title, value, type });
+
+    return response.json(newTransaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
