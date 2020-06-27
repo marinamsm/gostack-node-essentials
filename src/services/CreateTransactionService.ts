37b5d053f272108807931efa2726602c20feab0a
marinamsm <marinasouza.msm@gmail.com>
2020-06-27 11:00:45 -0300

    Service to create a new transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionCreation {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,36 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({
+    title,
+    value,
+    type,
+  }: TransactionCreation): Transaction | null {
+    const existentTransaction = this.transactionsRepository.findByWholeObject({
+      title,
+      value,
+      type,
+    });
+
+    if (existentTransaction) {
+      throw new Error('This transaction already exists');
+    }
+
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+
+      if (total - value < 0) {
+        throw new Error('Invalid Transaction');
+      }
+    }
+
+    const newTransaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return newTransaction;
   }
 }
 
